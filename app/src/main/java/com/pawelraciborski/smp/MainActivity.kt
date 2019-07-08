c0c78ef7367d5d5899a731c7727ff7f436dd6108
Paweł Raciborski <raciborski.p@gmail.com>
2019-07-08 14:34:23 +0200

    Adding submodule B

--- app/src/main/java/com/pawelraciborski/smp/MainActivity.kt
+++ app/src/main/java/com/pawelraciborski/smp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pawelraciborski.smp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pawelraciborski.moda.TestA
+import com.pawelraciborski.moda.TestB
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvContent.text = "${TestA.hello()}"
+        tvContent.text = "${TestA.hello()}/${TestB.hello()}"
     }
 }
