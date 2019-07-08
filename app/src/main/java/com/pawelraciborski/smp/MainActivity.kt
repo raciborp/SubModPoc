fac24be0df080710a8ca36e48e1792aabed8bc91
Paweł Raciborski <raciborski.p@gmail.com>
2019-07-08 14:27:25 +0200

    Utilizing modA

--- app/src/main/java/com/pawelraciborski/smp/MainActivity.kt
+++ app/src/main/java/com/pawelraciborski/smp/MainActivity.kt
@@ -2,11 +2,15 @@ package com.pawelraciborski.smp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pawelraciborski.moda.TestA
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvContent.text = "${TestA.hello()}"
     }
 }
